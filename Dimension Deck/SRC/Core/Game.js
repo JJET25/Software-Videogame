8c1675aa10deb5ec19d7b456a3757e14eea10719
gonzzv2006 <gonzzv2006@gmail.com>
2026-05-12 13:12:01 -0600

    Connected enemy system to game loop

--- Dimension Deck/SRC/Core/Game.js
+++ Dimension Deck/SRC/Core/Game.js
@@ -1,15 +1,34 @@
 import Player from "../Entities/Player.js";
+import Enemy from "../Entities/Enemy.js";
+
 import Vector from "../Utils/Vector.js";
+
 import Room from "../World/Room.js";
+
 import InputManager from "./Input.js";
 import Renderer from "./Renderer.js";
 
 export default class Game {
-    constructor(canvas) { 
+
+    constructor(canvas) {
+
         this.renderer = new Renderer(canvas);
+
         this.input = new InputManager();
+
         this.room = new Room();
-        this.player = new Player(new Vector(100, 100), this.input);
+
+        this.player = new Player(
+            new Vector(100, 100),
+            this.input
+        );
+
+        // Enemy list
+        this.enemies = [
+            new Enemy(new Vector(500, 300), this.player),
+            new Enemy(new Vector(700, 200), this.player)
+        ];
+
         this.lastTime = 0;
 
         this.renderer.resize();
@@ -19,25 +38,43 @@ export default class Game {
         this.start();
     }
 
-    start(){
+    start() {
         requestAnimationFrame((timestamp) => this.gameLoop(timestamp));
     }
 
-    gameLoop(timestamp){
+    gameLoop(timestamp) {
+
         const deltaTime = (timestamp - this.lastTime) / 1000;
+
         this.lastTime = timestamp;
 
         // Update
         this.renderer.setupResizeListener();
+
         this.player.update(deltaTime);
+
         this.room.update(deltaTime, this.player);
+
+        // Update enemies
+        for (let enemy of this.enemies) {
+            enemy.update(deltaTime);
+        }
+
+        // Remove dead enemies
+        this.enemies = this.enemies.filter(enemy => !enemy.isDead);
 
         // Clear
         this.renderer.clear();
 
         // Draw
         this.room.draw(this.renderer);
+
         this.player.draw(this.renderer);
+
+        // Draw enemies
+        for (let enemy of this.enemies) {
+            enemy.draw(this.renderer);
+        }
 
         requestAnimationFrame((timestamp) => this.gameLoop(timestamp));
     }
