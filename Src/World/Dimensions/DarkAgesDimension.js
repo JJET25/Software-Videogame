82d6ce86d4b984b71dd01112916dd762ea9170c9
gonzzv2006 <gonzzv2006@gmail.com>
2026-06-09 11:57:31 -0600

    added tank giant sprite

--- Src/World/Dimensions/DarkAgesDimension.js
+++ Src/World/Dimensions/DarkAgesDimension.js
@@ -2,16 +2,17 @@ import DungeonRat from "../../Entities/Enemies/DarkAgesEnemies/DungeonRat.js";
 import Skeleton from "../../Entities/Enemies/DarkAgesEnemies/Skeleton.js";
 import Spirit from "../../Entities/Enemies/DarkAgesEnemies/Spirit.js";
 import SkeletonKing from "../../Entities/Enemies/DarkAgesEnemies/SkeletonKing.js";
+import TwoHeadedGiant from "../../Entities/Enemies/DarkAgesEnemies/TwoHeadedGiant.js";
 import { ROOM_WEIGHTS } from "../../Utils/Constants.js";
 import Dimension from "./Dimension.js";
 import FallenKnight from "../../Entities/Enemies/DarkAgesEnemies/FallenKnight.js";
-
+ 
 const OBJECT_CONFIG = {
   rocks: { min: 1, max: 4 },
   boxes: { min: 0, max: 2 },
   spikes: { min: 0, max: 3 },
 };
-
+ 
 const DECORATIONS = {
   pool: {
     crack_1: { srcX: 16, srcY: 0, srcW: 16, srcH: 16 },
@@ -23,7 +24,7 @@ const DECORATIONS = {
   },
   frequency: 0.12,
 };
-
+ 
 export default class DarkAgesDimension extends Dimension {
   constructor() {
     super({
@@ -33,7 +34,7 @@ export default class DarkAgesDimension extends Dimension {
       tileSetId: "tilesDarkAge",
       enemyPool: {
         swarm: [DungeonRat],
-        tank: [Skeleton],
+        tank: [TwoHeadedGiant],   // Two Headed Giant — nuevo tank del dungeon
         ranged: [Spirit],
       },
       miniBoss: FallenKnight,
